b7a661ea4c77dffa45f5d0f27dacdb9d0362b04e
ti-metiss <coutenay.yoan@hotmail.fr>
2023-09-16 11:02:56 +0200

    Change color chart

--- src/app/shared/services/colors.service.ts
+++ src/app/shared/services/colors.service.ts
@@ -6,16 +6,18 @@ import { Injectable } from "@angular/core";
 export class ColorService {
 
     getNbColorRandom(numColor: number): string[]{
-
+        
+        let tabColor: string[] = [  '#793D52', '#89A1DB', '#9780A1', '#BFE0F1', '#B8CBE7','#956065',];
         let letters = '0123456789ABCDEF';
-        let tabColor: string[] = [];
-        while (numColor > 0){
-            let color = '#';
-            for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
+       if (numColor > 6){
+            while (numColor > 0){
+                let color = '#';
+                for (var i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                tabColor.push(color);
+                numColor--;
             }
-            tabColor.push(color);
-            numColor--;
         }
         return tabColor;
     }
